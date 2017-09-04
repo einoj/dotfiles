dc9bced79a50e791e1a4472623113600b7ce6598
Einoj <eino.juhani.oltedal@cern.ch>
2017-09-04 11:50:11 +0200

    setup now installs vim plugin vundle

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 cp vimrc ~/.vimrc
 cp bashrc ~/.bashrc
 source ~/.bashrc
+if hash git 2>/dev/null; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+else
+   echo "ERROR: cannot find command git.
+Install git to download vim plugin Vundle, and re-run script"
+fi
+
