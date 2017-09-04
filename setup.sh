96982cf0eacd1feecd415ab6dea9508ac2cea2a6
Einoj <eino.juhani.oltedal@cern.ch>
2017-09-04 12:20:18 +0200

    setup script now downloads and installs gruvbox color theme

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@ cp bashrc ~/.bashrc
 source ~/.bashrc
 if hash git 2>/dev/null; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    git clone https://github.com/morhetz/gruvbox.git ~/.vim/bundle/gruvbox
+    mkdir ~/.vim/colors
+    cp ~/.vim/bundle/gruvbox/colors/gruvbox.vim ~/.vim/colors/gruvbox.vim
 else
    echo "ERROR: cannot find command git.
 Install git to download vim plugin Vundle, and re-run script"
