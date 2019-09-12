3606027792cbeeac17c8c7614813a4fb427b6fb1
Eino J. Oltedal <einoju@gmail.com>
2019-09-12 12:19:03 +0200

    Fixed install script with error checking for vundle and expecting the E185 colorscheme error. Installation of git plugins is now wholly automatic

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -1,2 +1,15 @@
 cp ./vimrc ~/.vimrc
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo Installed new .vimrc in ~/
+
+if [ -d ~/.vim/bundle/Vundle.vim ]
+then
+    echo Vundle is already installed
+else
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+echo Installing plugins using vundle...
+# -E to ignore the E185 colorscheme not installed error
+echo Expecting E185
+vim -E -c "PluginInstall" -c ":q" -c ":q"
+echo Done!
