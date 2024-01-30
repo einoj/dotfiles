7c0d9918215f68c1a20ed6d94b10ea9e7da25ad9
Eino Juhani Oltedal <ejo@datarespons.no>
2024-01-30 10:50:47 +0100

    nvim: Make vim yank copy to clipboard and configure tabcomplete

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -2,8 +2,11 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.ignorecase = true
 vim.opt.swapfile = false
+vim.opt.clipboard = "unnamedplus"
 -- indent options
 vim.opt.smartindent = true
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+-- completion options
+vim.opt.wildmode = "longest,list,full"
