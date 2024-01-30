14d2f8ab17798dddfa1cd6eaf0357c3882562e4c
Eino Juhani Oltedal <ejo@datarespons.no>
2024-01-30 10:50:10 +0100

    nvim: Add shortcuts for hex editor and codejam

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -3,3 +3,6 @@ local opts = { noremap = true, silent = true}
 local keymap = vim.api.nvim_set_keymap
 
 keymap("i", "jj", "<esc>", opts)
+vim.keymap.set('n', '<leader>xx', ':%!xxd<CR>', {})
+vim.keymap.set('n', '<leader>xr', ':%!xxd -r<CR> :w<CR>', {})
+vim.cmd('abbr cjam #include <stdio.h><CR>#include <stdlib.h><CR><CR>int main(int argc, char* argv[]) {<CR>int i,j,k,N;<CR>scanf("%d", &N);<CR><CR>return 0;<CR>}')
